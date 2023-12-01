c711d43f3e155680dc37a3f64f16fbdea1461e48
twg23 <toluadetomiwa@gmail.com>
2023-12-01 08:52:22 +0000

    new updates

--- src/main/java/com/barclays/LibrarySystemAPI/service/ReserveServiceImpl.java
+++ src/main/java/com/barclays/LibrarySystemAPI/service/ReserveServiceImpl.java
@@ -25,6 +25,9 @@ public class ReserveServiceImpl implements ReserveService {
 
      PeriodicalRepository periodicalRepository;
 
+     ReservationStrategy reservationStrategy;
+
+
     List<Book> books= new ArrayList<>();
 
 
@@ -35,45 +38,15 @@ public class ReserveServiceImpl implements ReserveService {
     public ReservedItem save (ReserveDTO reserveDTO){
 
 
-        if(reserveDTO.getTitle().equals("MOVIE")) {
-            List<Movie> movies = movieRepository.searchByTitle(reserveDTO.getTitle());
-            if (movies.isEmpty()) throw new ItemNotFoundException("Item not found");
-
-            if (!(movies.get(0).getQuantity()>0)){
-                throw new ResponseStatusException(HttpStatusCode.valueOf(422),"Item not available");
-            }
-            else {
-                movies.get(0).setQuantity((movies.get(0).getQuantity())-1);
-                movieRepository.save(movies.get(0));
-            }
+        if(reserveDTO.getItemType().equals(ItemType.MOVIE)) {
+            reservationStrategy =new MovieReservationStrategy();
         }
 
-        if (reserveDTO.getTitle().equals("BOOK") ) {
-
-            List<Book> books = bookRepository.findByTitle(reserveDTO.getTitle());
-
-
-            if (books.isEmpty()) {
-
-                throw new ItemNotFoundException("Item not found");
-            }
-
-            log.debug("books quantity"  + books.get(0).getQuantity());
-
-            if (!(books.get(0).getQuantity()>0)){
-
-                throw new ResponseStatusException(HttpStatusCode.valueOf(422),"Item not available");
-            }
-            else {
-                books.get(0).setQuantity((books.get(0).getQuantity())-1);
-
-                bookRepository.save(books.get(0));
-            }
+        if (reserveDTO.getItemType().equals(ItemType.MOVIE) ) {
+            reservationStrategy = new BookReservationStrategy();
         }
-        if (reserveDTO.getTitle().equals("PERIODICAL")) {
-            List<Periodical> periodicals= periodicalRepository.searchByTitle(reserveDTO.getTitle());
-            if (periodicals.isEmpty()) throw new ItemNotFoundException("Item not found");
-        }
+
+        reservationStrategy.reserve(reserveDTO);
 
         User user = userRepository.findById(reserveDTO.getUserId()).
 
@@ -99,13 +72,14 @@ public class ReserveServiceImpl implements ReserveService {
 
 
     @Autowired
-    public ReserveServiceImpl( UserRepository userRepository, BookRepository bookRepository, MovieRepository movieRepository, ReserveRepository reserveRepository, PeriodicalRepository periodicalRepository) {
+    public ReserveServiceImpl(UserRepository userRepository, BookRepository bookRepository, MovieRepository movieRepository, ReserveRepository reserveRepository, PeriodicalRepository periodicalRepository, ReservationStrategy reservationStrategy, List<Book> books) {
         this.userRepository = userRepository;
         this.bookRepository = bookRepository;
         this.movieRepository = movieRepository;
         this.reserveRepository = reserveRepository;
         this.periodicalRepository = periodicalRepository;
+        this.reservationStrategy = reservationStrategy;
+        this.books = books;
     }
-
-    }
+}
 
