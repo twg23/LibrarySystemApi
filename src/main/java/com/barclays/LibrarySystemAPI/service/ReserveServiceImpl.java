4a1b541b65ffee361ec4fa77ebc5a607997a9379
twg23 <toluadetomiwa@gmail.com>
2024-01-19 15:38:13 +0000

    Hashmap of different strategy pattern for reserving item

--- src/main/java/com/barclays/LibrarySystemAPI/service/ReserveServiceImpl.java
+++ src/main/java/com/barclays/LibrarySystemAPI/service/ReserveServiceImpl.java
@@ -4,6 +4,7 @@ import com.barclays.LibrarySystemAPI.dto.ReserveDTO;
 import com.barclays.LibrarySystemAPI.exception.IdNotFoundException;
 import com.barclays.LibrarySystemAPI.model.*;
 import com.barclays.LibrarySystemAPI.repository.*;
+import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,10 @@ import org.springframework.stereotype.Service;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @Service
 @Slf4j
 @AllArgsConstructor
@@ -24,21 +28,33 @@ public class ReserveServiceImpl implements ReserveService {
      MovieReservationStrategy movieReservationStrategy;
      BookReservationStrategy bookReservationStrategy;
      List<Book> books= new ArrayList<>();
+     @Autowired(required = false)
+     Map<ItemType,ReservationStrategy> reservationStrategyMap;
+
+     @PostConstruct
+     public  void  init(){
+         reservationStrategyMap = new HashMap<>();
+         reservationStrategyMap.put(ItemType.MOVIE,movieReservationStrategy);
+         reservationStrategyMap.put(ItemType.BOOK,bookReservationStrategy);
+     }
 
     @Transactional
     @Override
     public ReservedItem save (ReserveDTO reserveDTO){
-        if(reserveDTO.getItemType().equals(ItemType.MOVIE)) {
-          //  reservationStrategy =new MovieReservationStrategy();
 
-            reservationStrategy=movieReservationStrategy;
-        }
+         reservationStrategy=reservationStrategyMap.get(reserveDTO.getItemType());
 
-        if (reserveDTO.getItemType().equals(ItemType.BOOK) ) {
-          //  reservationStrategy = new BookReservationStrategy();
-
-            reservationStrategy=bookReservationStrategy;
-        }
+//        if(reserveDTO.getItemType().equals(ItemType.MOVIE)) {
+//          //  reservationStrategy =new MovieReservationStrategy();
+//
+//            reservationStrategy=movieReservationStrategy;
+//        }
+//
+//        if (reserveDTO.getItemType().equals(ItemType.BOOK) ) {
+//          //  reservationStrategy = new BookReservationStrategy();
+//
+//            reservationStrategy=bookReservationStrategy;
+//        }
 
         reservationStrategy.reserve(reserveDTO);
 
